e1879840275a453de725091a1827bb4fb6a0c74c
hariom454 <thakurhariomsingh498@gmail.com>
2023-01-26 22:20:28 +0530

    Upgrade spring boot to 3 and add sample data in all users end point.

--- src/main/java/com/shashtra/openapidemo/controller/impl/SampleControllerImpl.java
+++ src/main/java/com/shashtra/openapidemo/controller/impl/SampleControllerImpl.java
@@ -3,6 +3,7 @@ package com.shashtra.openapidemo.controller.impl;
 import com.shashtra.openapidemo.controller.SampleController;
 import com.shashtra.openapidemo.controller.request.CreateUserRequest;
 import com.shashtra.openapidemo.dto.User;
+import java.util.UUID;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +17,13 @@ public class SampleControllerImpl implements SampleController {
   // TODO: implement methods to get sample data
   @Override
   public List<User> getAllUserDetails() {
-    return null;
+    //hard code user for testing purpose
+    User user = new User();
+    user.setUserId(UUID.randomUUID().toString());
+    user.setEmailAddress("fake.email@email.test");
+    user.setFirstName("fake.name");
+    user.setLastName("last.name");
+    return List.of(user);
   }
 
   @Override
